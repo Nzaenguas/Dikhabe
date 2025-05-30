b1abf7d831b3e229278bb60ced174a4720684fab
Engauchangbe Nza <nzaengaus@gmail.com>
2025-05-30 13:31:51 +0530

    final: deployment

--- app/(browse)/(home)/_components/results.tsx
+++ app/(browse)/(home)/_components/results.tsx
@@ -8,7 +8,7 @@ export const Results = async () => {
     return (
         <div>
             <h2 className="text-lg font-semibold mb-4">
-                Streams we think You&apos;ll need
+                Streams for you
             </h2>
             {data.length === 0 && (
                 <div className="text-muted-foreground text-sm">
